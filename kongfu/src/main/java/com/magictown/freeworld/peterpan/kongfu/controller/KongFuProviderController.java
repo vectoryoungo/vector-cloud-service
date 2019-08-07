2c74a758c84b08a4a1e879b2b1bdd6a1d16d70b5
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-07 15:35:46 +0800

    add test getSkill and getTeacher method

--- kongfu/src/main/java/com/magictown/freeworld/peterpan/kongfu/controller/KongFuProviderController.java
+++ kongfu/src/main/java/com/magictown/freeworld/peterpan/kongfu/controller/KongFuProviderController.java
@@ -7,6 +7,9 @@ package com.magictown.freeworld.peterpan.kongfu.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class KongFuProviderController {
 
@@ -14,6 +17,29 @@ public class KongFuProviderController {
     public String getKongFu() {
 
         return "BeatDragon";
+    }
+
+    @GetMapping(value = "/getSkill")
+    public List<String> getSkill() {
+
+        List<String> arrayList = new ArrayList<>(16);
+        arrayList.add("Swtter skill");
+        arrayList.add("Long Leg skill");
+        arrayList.add("Super voice skill");
+        arrayList.add("Penta kill skill");
+
+        return arrayList;
+    }
+
+    @GetMapping(value = "/getTeacher")
+    public List<String> getTeacher() {
+        List<String> teacherList = new ArrayList<>();
+        teacherList.add("Master wugui");
+        teacherList.add("Master Tiger");
+        teacherList.add("Master Monkey");
+        teacherList.add("Master Pig");
+
+        return teacherList;
     }
 }
 
