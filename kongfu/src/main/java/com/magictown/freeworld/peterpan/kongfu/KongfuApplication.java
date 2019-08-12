c483de3faa56eebc60765e06cf76fea672470135
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-12 15:22:55 +0800

    add hystrixdashboard

--- kongfu/src/main/java/com/magictown/freeworld/peterpan/kongfu/KongfuApplication.java
+++ kongfu/src/main/java/com/magictown/freeworld/peterpan/kongfu/KongfuApplication.java
@@ -3,9 +3,13 @@ package com.magictown.freeworld.peterpan.kongfu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
+@EnableHystrixDashboard
 public class KongfuApplication {
 
 	public static void main(String[] args) {
