226775383c67defe5e804f032aea2cdfa99669c5
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-12 17:51:34 +0800

    add amqp

---
+++ kongfu/src/main/java/com/magictown/freeworld/peterpan/kongfu/message/Receiver.java
@@ -0,0 +1,22 @@
+/**
+ * @create 2019-08-12 16:51
+ * @desc receiver
+ **/
+package com.magictown.freeworld.peterpan.kongfu.message;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(bindings = @QueueBinding(value = @Queue(value = "${vrabbitmq.config.queue.error}",autoDelete = "true"),
+exchange = @Exchange(value = "${vrabbitmq.config.exchange}",type = ExchangeTypes.DIRECT),key = "${vrabbitmq.config.queue.error.routing.key}"))
+public class Receiver {
+
+    @RabbitHandler
+    public void process(LogMessage logMessage) {
+        System.out.println("vector-cloud-service receive message " + logMessage.toString());
+    }
+
+}
+
