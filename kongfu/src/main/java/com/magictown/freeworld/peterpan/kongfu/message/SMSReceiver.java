a6bc010d276e1b2feb0e6fd2351b34a0f7e1554a
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-13 14:49:02 +0800

    add fanout message

---
+++ kongfu/src/main/java/com/magictown/freeworld/peterpan/kongfu/message/SMSReceiver.java
@@ -0,0 +1,24 @@
+/**
+ * @create 2019-08-13 14:37
+ * @desc sms process
+ **/
+package com.magictown.freeworld.peterpan.kongfu.message;
+
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@RabbitListener(
+        bindings = @QueueBinding(value = @Queue(value = "order.sms",autoDelete = "true"),
+                exchange = @Exchange(value ="${vrabbitmq.config.fanout.exchange}",type = ExchangeTypes.FANOUT))
+)
+public class SMSReceiver {
+
+    @RabbitHandler
+    public void process(String message) {
+        System.out.println("already receive sms message " + message);
+    }
+
+}
+
